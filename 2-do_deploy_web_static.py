d6b28280ea562ddf9cd4530e9b1670dd781533f7
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-04-08 17:08:55 -0700

    file created

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,52 +1,67 @@
 #!/usr/bin/python3
-""" Fabric script to distribute an archive to a web server."""
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
-from os.path import exists
+""" 0x03. AirBnB clone - Deploy static, task 3. Full deployment
+"""
+from fabric.api import local, env, put, run
+from os import path, makedirs
+from datetime import datetime
 
-env.hosts = ["100.26.132.166", "100.25.179.168"]
-env.user = "ubuntu"
+env.hosts = ['100.26.132.166', '100.25.179.168']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """ Generates a .tgz archive from the contents `web_static/`
+
+    Retruns:
+        (str): full path from current directory to `.tgz` archive created in
+    `versions/`, or `None` on failure
+    """
+    if not path.isdir("./versions"):
+        makedirs("./versions")
+
+    now = datetime.now().strftime('%Y%m%d%H%M%S')
+    local('tar -cvzf versions/web_static_{}.tgz web_static/'.format(now))
+
+    if not path.exists('./versions/web_static_{}.tgz'.format(now)):
+        return None
+    return path.realpath('./versions/web_static_{}.tgz'.format(now))
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """ Distributes a .tgz archive from the contents of `web_static/` in AirBnB
+    clone repo to the web servers
 
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+    Retruns:
+        (bool): `True` if all operations successful, `False` otherwise
     """
-    if not exists(archive_path):
+    if not path.exists(archive_path) or archive_path is None:
         return False
 
-    try:
-        # Upload the archive to /tmp/ directory on the web servers
-        put(archive_path, '/tmp/')
+    f_name = path.basename(archive_path)
+    d_name = f_name.split('.')[0]
 
-        # Extract archive to /data/web_static/releases/
-        archive_name = archive_path.split('/')[-1]
-        folder_name = archive_name.split('.')[0]
-        release_path = '/data/web_static/releases/{}'.format(folder_name)
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_path))
+    put(local_path=archive_path, remote_path='/tmp/')
+    run('mkdir -p /data/web_static/releases/{}/'.format(d_name))
+    run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+        f_name, d_name))
+    run('rm /tmp/{}'.format(f_name))
+    run('mv /data/web_static/releases/{}/web_static/* '.format(d_name) +
+        '/data/web_static/releases/{}/'.format(d_name))
+    run('rm -rf /data/web_static/releases/{}/web_static'.format(d_name))
+    run('rm -rf /data/web_static/current')
+    run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(
+        d_name))
+    return True
 
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(archive_name))
 
-        # Move contents of the extracted folder to the releases folder
-        run('mv {}/web_static/* {}'.format(release_path, release_path))
+def deploy():
+    """ First generates a .tgz archive from current contents of `web_static/`,
+    then deploys it to update the same dir in two remote web servers.
 
-        # Remove the symbolic link /data/web_static/current
-        current_link = '/data/web_static/current'
-        if exists(current_link):
-            run('rm -rf {}'.format(current_link))
-
-        # Create a new symbolic link linked to the new version of code
-        run('ln -s {} {}'.format(release_path, current_link))
-
-        return True
-    except Exception as e:
-        print(e)
+    Retruns:
+        (bool): `True` if all operations successful, `False` otherwise
+    """
+    path = do_pack()
+    if path is None:
         return False
+    return(do_deploy(path))
