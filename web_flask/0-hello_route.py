3b5db5156669fc57d922cb85777480b09c445690
Hasna <hasna.elboukhari.neuro@gmail.com>
2024-08-24 12:26:30 +0000

    file created

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+"""Simple Flask web application"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
+
+@app.route('/airbnb-onepage/')
 def hello_hbnb():
-    return 'Hello HBNB!'
+    """Returns a string at the /airbnb-onepage/ route"""
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host="0.0.0.0", port=5000)
